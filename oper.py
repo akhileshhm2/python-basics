cb76e4eb7c0e1480ab75412a331cb247a5120537
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-10 12:12:31 +0530

    Add files via upload

---
+++ oper.py
@@ -0,0 +1,44 @@
+# x="akhilesh"
+# y="HM"
+# print(x+y)
+
+# num1=5
+# num1 += 3
+# print(num1)
+
+# num2=5
+# num2 -=3
+# print(num2)
+
+# num3=5
+# num3 **= 2
+# print(num3)
+
+# num1=5
+# num2=10
+# print(num1 <num2)
+
+# num1=10
+# print(num1>15 and num1>5)
+
+# num2=20
+# print(num2>15 or num2>30)
+
+# num3=30
+# print(not(num3>15 and num3>20))
+
+# x=["orange","apple"]
+# y=["coco","chilly"]
+# z=x
+# print(y is z)
+
+# x=["orange","apple"]
+# y=["coco","chilly"]
+# print("orange" in x)
+
+x=["orange","apple"]
+y=["coco","chilly"]
+print("orange"not in x)
+
+
+
---
+++ py basic.py
@@ -0,0 +1,34 @@
+# name1="akhilesh"
+# print(name1)
+
+# num2=123
+# print(num2)
+
+# des3=12.2
+# print(des3)
+
+# str2 ="akhilesh"
+# print(str2)
+# print(type(str2))
+
+# name="001"
+# dig=float(name)
+# print(type(dig))
+# print(dig)
+
+# id="akhil"
+# name=tuple(id)
+# print(type(name))
+# print(id)
+# print(name)
+
+flt=1.22
+print(type(flt))
+dig=int(flt)
+print(dig)
+print(type(dig))
+
+
+
+
+
