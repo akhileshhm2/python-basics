3bde37d5aaa3e49c92aa17491d9a6b7821054478
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-16 16:20:05 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,59 @@
+# set1={"apple","cherry","mango",True,False,0}
+# print(set1)
+
+# set1={"apple","cherry","mango",}
+# print("apple"in set1)
+
+# set1={"apple","cherry","mango",True,False,0}
+# set1.add("orange")
+# print(set1)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange"}
+# set1.update(set2)
+# print(set1)
+
+# set1={"apple","cherry","mango"}
+# list1=["berry","orange"]
+# set1.update(list1)
+# print(set1)
+
+# set1={"apple","cherry","mango"}
+# set1.remove("apple")
+# print(set1)
+
+# set1={"apple","cherry","mango"}
+# x=set1.pop()
+# print(set1)
+# print(x)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange"}
+# set3=set1 | set2
+# print(set3)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange","apple"}
+# set3=set1 & set2
+# print(set3)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange"}
+# set3= set1-set2
+# print(set3)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange","apple"}
+# set3= set1 ^ set2
+# print(set3)
+
+# set1={"apple","cherry","mango"}
+# set2={"berry","orange",}
+# set3=set1.isdisjoint(set2)
+# print(set3)
+
+
+
+
+
+
---
+++ tupple.py
@@ -0,0 +1,43 @@
+# list1=("orange","apple","mango","cherry")
+# y=list(list1)
+# y[1]="kiwi"
+# list1=tuple(y)
+# print(list1)
+
+# list1=("orange","apple","mango","cherry")
+# y=list(list1)
+# y.append("berry")
+# list1=tuple(y)
+# print(list1)
+
+# list1=("orange","apple","mango","cherry")
+# list2=("chilly","coconut")
+# x=list1+list2
+# print(x)
+
+# list1=("orange","apple","mango","cherry")
+# x=list(list1)
+# x.remove("apple")
+# list1=tuple(x)
+# print(list1)
+
+# list1=("orange","apple","mango","cherry")
+# del list1
+# print(list1)
+
+# list1=("orange","apple","mango","cherry")
+# list2=2*list1
+# print(list2)
+
+# list1=("orange","apple","mango","cherry")
+# y=list1.count("apple")
+# print(y)
+
+# list1=(1,3,2,4,34,3,5,4,5,43,2,4,5,7,78,6,5,7,6,8,0)
+# y=list1.index(3)
+# print(y)
+
+# list1=("orange","apple","mango","cherry")
+# y=("orange",)
+# list1 += y
+# print(list1)
