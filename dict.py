488d0fd18df84db9aa181234059ce845f9f0627b
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-17 14:17:42 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,89 @@
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# x=dict1["model"]
+# print(x)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# x=dict1.keys()
+# print(x)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# x=dict1.values()
+# print(x)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# x=dict1.items()
+# print(x)    #items op will be in tuple
+
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# x=dict1.items()
+# print(x)
+# dict1["Brand"]="kia"
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# dict1["engine"]=2.0
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# dict1.update({"model":"seltos"})
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# dict1.pop("model")
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# dict1.popitem()
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# del dict1["model"]
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# del dict1
+# print(dict1)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# for i in dict1:
+#     print(i)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# for i in dict1.values():
+#     print(i)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# for i in dict1.keys():
+#     print(i)
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black"}
+# for x,y in dict1.items():
+#     print(x,y)
+
+# car={"car1":{
+#         "Brand":"kia",
+#         "model":"seltos",
+#         "color":"white"
+#      },
+#      "car2":{
+#          "Brand":"hyundai",
+#          "Model":"creta",
+#          "color":"black"
+#      },
+#      "car3":{
+#          "Brand":"honda",
+#          "model":"amaze",
+#          "color":"red"
+
+#      }
+#      }
+# print(car)
+# print(car["car2"])
+# print(car["car3"]["model"])
+
+
+
