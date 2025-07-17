65c54509da84cc96809416fdc936f420371a7973
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-17 15:34:12 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,32 @@
+# def my_fun():
+#     print("hello world")
+# my_fun()    
+    
+# def name(f_name):
+#     print(f_name+" chattergi")
+# name("kellian")
+# name("dakku")
+# name("lather")    
+
+# def sum(a,b):
+#     print(a+b)
+# sum(2,3)    
+
+# def sum(a=5,b=10):
+#     sum=a+b
+#     print("sum=",sum)
+# sum(2,3)    
+# sum()
+
+# def dis_in(first_name,last_name):
+#     print("firstname",first_name)
+#     print("secondname",last_name)
+# dis_in(last_name="mahesh",first_name="akhilesh")
+
+# def dis_in(*numbers):
+#     result=0
+#     for num in numbers:
+#      result=result+num
+#      print("sum=",result)
+# dis_in(1,2,3)    
+
