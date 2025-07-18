56fd9e5c94f7220b7ec6e72856672ad92fc26bb5
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-18 20:51:40 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,122 @@
+# list1=[1,3,5,7]
+# for i in list1:
+#     i +=i
+# print(i)
+
+# list1=[1,3,4,5,8]
+# list2=[3,2,5,8]
+# x=set(list1)|set(list2)
+# y=set(list1)&set(list2)
+# list1=list(x)
+# list2=list(y)
+# print(list1)
+# print(list2)
+
+# list1=[1,3,2,4,8]
+# list2=tuple(list1)
+# print(list2)
+# list3=(1,3,4,2,1)
+# list4=list(list3)
+# print(list4)
+
+# tuple1=(1,3,4,3,4,3,45,3)
+# x=int(input("enter the number:"))
+# for i in tuple1:
+#     if x in tuple1:
+#         count=tuple1.count(x)
+# print(x)
+
+# set1={1,2,5,7,9,10}
+# set2={3,7,8,10.2}
+# print(f"intersection:",set1&set2)
+# print(f"union:",set1|set2)
+# print(f"difference",set1-set2)
+
+# set1={1,2,5,7,9,10}
+# set2={8}
+# if set1&set2:
+#     print("not disjoint")
+# else:
+#     print("disjoint") 
+
+# set1={1,2,5,7,9,10}
+# set1.add(11)
+# print(set1)
+# set1.remove(2)
+# print(set1)
+
+# dict1={"car":"kia"}
+# dict2={"bike":"honda"}
+# dict1.update(dict2)
+# print(dict1)
+
+# n=int(input("enter the limit"))
+# usr_list=[]
+# for i in range (n):
+#     elements=input("enter elements:")
+#     usr_list.append(elements)
+# print(usr_list)   
+# 
+# list1=["car","bike","scooty"]
+# list1.append("plane")
+# print(list1)
+# list1.insert(1,"jet")
+# print(list1)
+# list1.pop(1)
+# print(list1)
+# del list1[2]
+# print(list1)
+# list1.remove("plane")
+# print(list1)
+    
+# tup1=(1,2,"akhilesh","ragav",9,"pradabh")
+# tup2=("ram","gopi",4,8)
+# print(tup1[2])
+# list1=["car","bike","scooty"]
+# x=tuple(list1)
+# print(x)
+# list1=list(x)
+# print(list1)
+# res=tup1+tup2
+# print(res)
+# print(len(tup1))
+# for x in tup1:
+#     if x=="gopi":
+#      print("present")
+#      break
+# else:
+#      print("not present")
+    
+
+# dict1={"Brand":"Ford","model":"Mustang","color":"black","year":"2024","version":"top"}
+# x=dict1["Brand"]
+# print(x)
+# dict1["version"]="mid"
+# print(dict1)
+# dict1["engine"]=2.0
+# print(dict1)
+# dict1.pop("version")
+# print(dict1)
+
+# n=int(input("enter the limit:"))
+# in_set=set()
+# for i in range(n):
+#     num=int(input("enter the elements:"))
+#     in_set.add(num)
+# print(in_set)
+# in_set.add(6)
+# print(in_set)
+# in_set.update([2,3])
+# print(in_set)
+# in_set.remove(1)
+# print(in_set)
+# in_set.discard(2)
+# print(in_set)  
+# 
+# set1={1,3,5,7,9}
+# set2={3,2,6,9,10}
+# set3=set1 ^ set2
+# print(set3) 
+ 
+    
+
