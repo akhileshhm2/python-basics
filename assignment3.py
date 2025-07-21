35a97b7c4c3b7bff25af3a4f7a9f31c4457aeee8
akhileshhm2 <akhileshmahesh00@gmail.com>
2025-07-21 21:23:50 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,60 @@
+# def squr(a):
+#     return a*a
+# x=int(input("enter the number:"))
+# print("square of the given number:",squr(x))
+
+# def checker(a):
+#     if a%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# x=int(input("enter the number:"))
+# checker(x)   
+ 
+# def add(x,y):
+#     return x+y
+# def sum():
+#     num1=int(input("enter the 1st number:"))
+#     num2=int(input("enter the second number:"))
+#     print("result=",add(num1,num2))
+# sum()  
+ 
+# def factorial(n):
+#     fact=1
+#     for i in range (1,n+1):
+#         fact *=i
+#     return fact
+# num=int(input("enter the number:"))
+# if num<1:
+#     print("negative numbers are not defined")
+# else:
+#     print(f"factorial of {num} is ",factorial(num))
+
+# def is_palindrome(s):
+#     return s==s[::-1]
+# name=input("enter the string:")
+# if is_palindrome(name):
+#     print("its a palindrome")
+# else:
+#     print("not a palindrome")
+# # 
+# def largest(number):
+#     larg_num=number[0]
+#     for num in number:
+#         if num>larg_num:
+#             larg_num=num
+#     return larg_num
+# list1=[2,6,10,8,3]
+# print("the latgest num is:",largest(list1))           
+
+
+
+
+
+
+
+           
+
+
+
+
